a2714e90a319f7be16f3b661b5648f36c727092e
gabrielcruz <garusotec@gmail.com>
2021-04-13 01:01:10 -0300

    Upgrade view

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import os
-from flask import Flask, request
+from flask import Flask, json, request, Response
 
 app = Flask(__name__)
 app.config.update(TESTING=True, SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/')
 directory_paths = os.listdir("functions")
 
 
-@app.route("/<string:var>")
+@app.route("/<string:var>", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 def make_view(var):
     exec(f"from functions.{var} import main")
     function = eval("main")
     response = function.execute()
-    if not function.execute():
-        return ("BAD REQUEST", 400)
+    if not response:
+        return Response(
+            status=400,
+        )
 
-    return (response, 200)
+    return Response(
+        response=json.dumps(response),
+        mimetype="application/json",
+        status=200,
+    )
 
 
 if __name__ == "__main__":
